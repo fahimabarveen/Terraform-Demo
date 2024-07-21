e4c79e84c908e3f220489825df46c8480632db43
Fahima <fahimasherinaz@gmail.com>
2024-07-21 14:15:19 +0530

    baseline

---
+++ TerraformCode/Lab3/output.tf
@@ -0,0 +1,13 @@
+
+output "resource_group_id" {
+  value = azurerm_resource_group.rg1.id
+}
+
+output "location" {
+  value = azurerm_resource_group.rg1.location
+}
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg1.name
+
+}
